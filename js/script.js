7b5e8f455a269949b855f544803124d5a1184fee
sashapoliti <sas.politi@gmail.com>
2024-04-08 15:23:39 +0200

    add method getRandomMail with axios.get done

--- js/script.js
+++ js/script.js
@@ -3,13 +3,19 @@ const { createApp } = Vue;
 createApp({
   data() {
     return {
+      maxMail : 10
     };
   },
   methods: {
+    getRandomMail() {
+      axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((response) => {
+        console.log(response);
+      })
+    }
   },
-  computed: {
-  },
-  mounted() {
-
-  },
+  computed: {},
+  mounted() {},
+  created() {
+    this.getRandomMail();
+  }
 }).mount("#app");
